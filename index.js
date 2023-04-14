94dce9b8a2b2d52f0835b7a52655d486da01fb11
daniloherreraf <herreraf.danilo@gmail.com>
2023-04-14 11:26:39 -0500

    precargar especialidades

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const app = require("./src/app");
 const port = 3001;
-const { sequelize } = require("./src/db");
+const specialties = require("./src/arraySpecialtiesTemplate.js")
+const { sequelize, Specialty } = require("./src/db");
 
 app.listen(port, () => {
   sequelize.sync({ force: true });
   console.log(`App listening on port ${port}`);
+
+  specialties.forEach(async (element)=> await Specialty.create({name: element}));
+
+  console.log("Types specialties pre-loaded")
 });
