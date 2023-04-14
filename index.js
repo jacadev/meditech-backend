ac76b5b68c439bb2071a4cb7d30eb3e93b8a8e95
AdrianZarate <adrian.1478.rz@gmail.com>
2023-04-14 12:17:17 -0500

    precargado de especialidades

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const app = require("./src/app");
 const port = 3001;
-const specialties = require("./src/arraySpecialtiesTemplate.js")
+const {preloadSpecialtiesHelper} = require("./src/helpers")
 const { sequelize, Specialty } = require("./src/db");
 
-app.listen(port, () => {
-  sequelize.sync({ force: true });
-  console.log(`App listening on port ${port}`);
+app.listen(port, async () => {
 
-  specialties.forEach(async (element)=> await Specialty.create({name: element}));
+  sequelize.sync({ force: true }).then( async () => {
+    await Specialty.bulkCreate(preloadSpecialtiesHelper)
+  });
+
+  console.log(`App listening on port ${port}`);
 
   console.log("Types specialties pre-loaded")
 });
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -4,6 +4,7 @@ const objTemplateHelper = require('./objTemplate.helper');
 const orderByNameHelper = require('./orderByName.helper');
 const orderByRatingHelper = require('./orderByRating.helper');
 const paginationHelper = require('./pagination.helper');
+const preloadSpecialtiesHelper = require('./preloadSpecialties.helper')
 
 
 module.exports = {
@@ -12,5 +13,6 @@ module.exports = {
   objTemplateHelper,
   orderByNameHelper,
   orderByRatingHelper,
-  paginationHelper
+  paginationHelper,
+  preloadSpecialtiesHelper
 };
