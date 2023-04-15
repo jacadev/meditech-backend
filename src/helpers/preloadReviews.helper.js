4c5046013a37429194b21b901316dbd19fdec51b
Jesus <jaca-btc@gmail.com>
2023-04-15 10:53:49 -0500

    actualizado modelos de appointment

---
+++ src/helpers/preloadReviews.helper.js
@@ -0,0 +1,10 @@
+const reviews = [  
+  {
+    author: "Petra Perez",
+    comment: "Excelente profesional me gustado mucho su consulta",
+    comment_date: "25/01/2023",
+    rating: 5,
+  },
+]
+
+module.exports = rol;
