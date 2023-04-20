ec9bd2a5e6ac0b90e6937121380e8e0e54137d37
Nicolas <nicolas_flores22@outlook.com>
2023-04-19 21:40:47 -0500

    validando de que no se pueda guardar dos veces el mismo horario en el mismo día

--- src/controllers/disponibilties/postDisponibilty.controller.js
+++ src/controllers/disponibilties/postDisponibilty.controller.js
@@ -4,6 +4,17 @@ const postDisponibiltyController = async (date, day_id, timetable_id, doctor_id)
 
     if (!date || !day_id || !timetable_id || !doctor_id) throw new Error("Faltan datos obligatorios");
 
+    const existingDisponibilty = await Disponibilty.findOne({
+        where: {
+            day_id,
+            timetable_id,
+        },
+    });
+
+    if (existingDisponibilty) {
+        throw new Error("Este horario ya está disponible para este día");
+    }
+
     const disponibilty = await Disponibilty.create({
         date,
         day_id,
