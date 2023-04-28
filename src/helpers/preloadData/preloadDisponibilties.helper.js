7798aecfd31a1d83c88329faddde00537d2ca222
Nicolas <nicolas_flores22@outlook.com>
2023-04-28 14:26:04 -0500

    agregué disponibilidades a los doctores que no tenían. Modifiqué el preloadDisponibilties

--- src/helpers/preloadData/preloadDisponibilties.helper.js
+++ src/helpers/preloadData/preloadDisponibilties.helper.js
@@ -58,6 +58,102 @@ const preloadDisponibilties = [
         day_id: 7,
         timetable_ids: [1, 2, 9],
         doctor_id: 5
+    },
+    {
+        date: "2023-06-19",
+        day_id: 2,
+        timetable_ids: [2, 5, 6],
+        doctor_id: 11
+    },
+    {
+        date: "2023-07-12",
+        day_id: 3,
+        timetable_ids: [3, 7, 9],
+        doctor_id: 12
+    },
+    {
+        date: "2023-05-24",
+        day_id: 5,
+        timetable_ids: [1, 7, 8],
+        doctor_id: 13
+    },
+    {
+        date: "2023-06-03",
+        day_id: 4,
+        timetable_ids: [4, 5, 6],
+        doctor_id: 14
+    },
+    {
+        date: "2023-06-17",
+        day_id: 6,
+        timetable_ids: [2, 8, 9],
+        doctor_id: 15
+    },
+    {
+        date: "2023-06-08",
+        day_id: 1,
+        timetable_ids: [1, 3, 7],
+        doctor_id: 16
+    },
+    {
+        date: "2023-07-04",
+        day_id: 4,
+        timetable_ids: [5, 6, 7],
+        doctor_id: 17
+    },
+    {
+        date: "2023-06-30",
+        day_id: 5,
+        timetable_ids: [2, 4, 9],
+        doctor_id: 18
+    },
+    {
+        date: "2023-05-23",
+        day_id: 3,
+        timetable_ids: [2, 3, 4],
+        doctor_id: 19
+    },
+    {
+        date: "2023-06-15",
+        day_id: 7,
+        timetable_ids: [1, 2, 9],
+        doctor_id: 20
+    },
+    {
+        date: "2023-07-06",
+        day_id: 1,
+        timetable_ids: [4, 7, 8],
+        doctor_id: 21
+    },
+    {
+        date: "2023-05-27",
+        day_id: 2,
+        timetable_ids: [1, 5, 6],
+        doctor_id: 22
+    },
+    {
+        date: "2023-06-26",
+        day_id: 3,
+        timetable_ids: [3, 7, 9],
+        doctor_id: 23
+    },
+    {
+        date: "2023-07-02",
+        day_id: 5,
+        timetable_ids: [1, 7, 8],
+        doctor_id: 24
+    },
+    {
+        date: "2023-06-10",
+        day_id: 4,
+        timetable_ids: [4, 5, 6],
+        doctor_id: 25
+    },
+    {
+        date: "2023-05-18",
+        day_id: 6,
+        timetable_ids: [2, 8, 9],
+        doctor_id: 26
     }
 ];
 
