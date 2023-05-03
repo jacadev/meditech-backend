5085513e21c0d3c669697aa496ff479dc2fe0e42
daniloherreraf <herreraf.danilo@gmail.com>
2023-05-03 17:04:46 -0500

    cambios-codigo

--- src/handlers/patients/postSignin.handler.js
+++ src/handlers/patients/postSignin.handler.js
@@ -5,26 +5,23 @@ const { generateToken } = require('../../helpers/utils.herlper');
 const postSigninHandler = async (req, res) => {
   const user = await Person.findOne({ where: { email: req.body.email } });
 
-  try {
-    if (user && user.status === true) {
-      const patient = await Patient.findOne({
-        where: { person_id: user.id },
-        include: [{ model: Person, attributes: ['id'] }],
-      });
+  if (user && user.status === true) {
+    const patient = await Patient.findOne({
+      where: { person_id: user.id },
+      include: [{ model: Person, attributes: ['id'] }],
+    });
 
-      if (bcrypt.compareSync(req.body.password, user.password)) {
-        return res.send({
-          id: patient.id,
-          user_name: user.userName,
-          email: user.email,
-          rol: user.rol_id,
-          token: generateToken(user),
-        });
-      }
+    if (bcrypt.compareSync(req.body.password, user.password)) {
+      return res.send({
+        id: patient.id,
+        user_name: user.userName,
+        email: user.email,
+        rol: user.rol_id,
+        token: generateToken(user),
+      });
     }
-  } catch (error) {
-    return res.status(401).json({ error: error.message });
   }
+  return res.status(401).send({ message: 'Invalid email or password' });
 };
 
 module.exports = postSigninHandler;
