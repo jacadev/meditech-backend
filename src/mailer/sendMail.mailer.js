89b873f6ef096019bb7478efff01f047ba4d216a
daniloherreraf <herreraf.danilo@gmail.com>
2023-05-03 16:52:02 -0500

    modificaciones codigo

--- src/mailer/sendMail.mailer.js
+++ src/mailer/sendMail.mailer.js
@@ -1,31 +1,16 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
 
 const sendMail = async (userData, template) => {
-   try {
-  //   let transporter = nodemailer.createTransport({
-  //     host: "smtp-relay.sendinblue.com",
-  //     port: 587,
-  //     secure: false, // true for 465, false for other ports
-  //     auth: {
-  //       user: "jesus@etereoworks.com", // generated ethereal user
-  //       pass: "8dxgbp0KIPJNQTCq", // generated ethereal password
-  //     },
-  //   });
-
-  let transporter = nodemailer.createTransport({
-    host: 'smtp-relay.sendinblue.com',
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: 'jesus@etereoworks.com', // generated ethereal user
-      pass: '8dxgbp0KIPJNQTCq', // generated ethereal password
-    },
-    tls: {
-      rejectUnauthorized: false
-    }
-  });
-
-  
+  try {
+    let transporter = nodemailer.createTransport({
+      host: 'smtp-relay.sendinblue.com',
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: 'jesus@etereoworks.com', // generated ethereal user
+        pass: '8dxgbp0KIPJNQTCq', // generated ethereal password
+      },
+    });
 
     let templateToSend = template(userData);
 
@@ -37,14 +22,14 @@ const sendMail = async (userData, template) => {
       html: templateToSend, // html body
     });
 
-    console.log("Message sent: %s", info.messageId);
+    console.log('Message sent: %s', info.messageId);
     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
     // Preview only available when sending through an Ethereal account
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
     // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
   } catch (error) {
-    console.log("An error occured...");
+    console.log('An error occured...');
     console.error(error);
     throw new Error(error); // handle errors
   }
