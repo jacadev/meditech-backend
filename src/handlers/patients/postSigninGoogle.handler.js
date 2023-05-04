882a33eb46f3e24c3d0b92efca28bb187f2cba18
Jesus <jaca.btc@gmail.com>
2023-05-04 11:39:08 -0500

    rollback al mailer en inicio de sesion con google

--- src/handlers/patients/postSigninGoogle.handler.js
+++ src/handlers/patients/postSigninGoogle.handler.js
@@ -1,9 +1,6 @@
 const { Patient, Person } = require("../../db");
 const postPatientGoogleController = require("../../controllers/patients/postPatientGoogle.controller");
 
-const sendMail = require("../../mailer/sendMail.mailer");
-const signupMessageHtml = require("../../mailer/templates/signupMessage.template");
-
 const postSigninGoogleHandler = async (req, res) => {
   const { user_name, email, rol } = req.body;
   try {
@@ -18,21 +15,12 @@ const postSigninGoogleHandler = async (req, res) => {
       include: [{ model: Person, attributes: ["id"] }],
     });
 
-    res.send({
+    return res.send({
       id: patient.id,
       user_name: patientPosted.user_name,
       first_name: patientPosted.first_name,
       email: patientPosted.email,
-    });
-
-    let mailInfo = {
-      firstName: patientPosted.user_name,
-      email: patientPosted.email,
-      fullName: patientPosted.user_name,
-      subject: "¡Bienvenido a Meditech!",
-    };
-
-    await sendMail(mailInfo, signupMessageHtml);
+    });  
 
   } catch (error) {
     return res.status(404).json({ error: error.message });
